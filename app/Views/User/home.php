feb0398ca3a177aedff2dc126286604a3dc7031b
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2023-05-13 18:44:54 +0700

    the frontend of homepage is finished

--- app/Views/User/home.php
+++ app/Views/User/home.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" type="text/css" href="/css/user/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
 </head>
 
@@ -43,28 +44,103 @@
                 <table class="table">
                     <tbody>
                         <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                            <th scope="row">1</th>
+                            <td>
+                                <img class="ava-lead" src="/images/avatar.jpg" >
+                                <span>@antonioH</span>
+                            </td>
+                            <td class="coin-amount">1020c</td>
                         </tr>
                         <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                            <th scope="row">2</th>
+                            <td>
+                                <img class="ava-lead" src="/images/avatar.jpg" >
+                                <span>@agustianP</span>
+                            </td>
+                            <td class="coin-amount">900c</td>
                         </tr>
                         <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                            <th scope="row">3</th>
+                            <td>
+                                <img class="ava-lead" src="/images/avatar.jpg" >
+                                <span>@roseeLes</span>
+                            </td>
+                            <td class="coin-amount">880c</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>
+                                <img class="ava-lead" src="/images/avatar.jpg" >
+                                <span>@Bayuuu</span>
+                            </td>
+                            <td class="coin-amount">870c</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">5</th>
+                            <td>
+                                <img class="ava-lead" src="/images/avatar.jpg" >
+                                <span>@agustianP</span>
+                            </td>
+                            <td class="coin-amount">850c</td>
                         </tr>
                     </tbody>
                 </table>
+                <a class="link-offset-2 link-underline link-underline-opacity-0 load-more" href="#">Load More...</a>
+                <div class="clear"></div>
             </div>
         </div>
-        <div class="col-lg-8">
-            
+        <div class="col-lg-8 right-content">
+            <h2>Welcome to GRAPEPro</h2>
+            <h5>Pemula</h5>
+            <div class="box-class">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <div class="level"><p>Lvl. 1</p></div>
+                        </div>
+                        <div class="col-lg-10 class-section">
+                            <span class="class-title">Pengenalan Pemrograman</span><i class="bi bi-calendar-check icon-status"></i>
+                            <span class="class-desc">Lorem ipsum dolor sit amet, consectetur.</span>
+                            <div class="info-class">
+                                <span><i class="bi bi-file-text"></i>&nbsp;&nbsp;5 Artikel &nbsp;&nbsp;| &nbsp;&nbsp;<i class="bi bi-clipboard2-check"></i>&nbsp;&nbsp;1 Ujian &nbsp;&nbsp;| &nbsp;&nbsp;1x Pengulangan</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box-class">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <div class="level"><p>Lvl. 2</p></div>
+                        </div>
+                        <div class="col-lg-10 class-section">
+                            <span class="class-title">Pemrograman Struktur</span><i class="bi bi-lock icon-status"></i>
+                            <span class="class-desc">Lorem ipsum dolor sit amet, consectetur.</span>
+                            <div class="info-class">
+                                <span><i class="bi bi-file-text"></i>&nbsp;&nbsp;5 Artikel &nbsp;&nbsp;| &nbsp;&nbsp;<i class="bi bi-clipboard2-check"></i>&nbsp;&nbsp;1 Ujian &nbsp;&nbsp;| &nbsp;&nbsp;0x Pengulangan</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h5>Menengah</h5>
+            <div class="box-class">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <div class="level"><p>Lvl. 3</p></div>
+                        </div>
+                        <div class="col-lg-10 class-section">
+                            <span class="class-title">Kendali Pemrograman</span><i class="bi bi-lock icon-status"></i>
+                            <span class="class-desc">Lorem ipsum dolor sit amet, consectetur.</span>
+                            <div class="info-class">
+                                <span><i class="bi bi-file-text"></i>&nbsp;&nbsp;5 Artikel &nbsp;&nbsp;| &nbsp;&nbsp;<i class="bi bi-clipboard2-check"></i>&nbsp;&nbsp;1 Ujian &nbsp;&nbsp;| &nbsp;&nbsp;0x Pengulangan</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
